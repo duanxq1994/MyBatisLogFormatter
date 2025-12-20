88fa7ce68507e45bb3cbb918693d3f316e107d7d
duanxq <xqduan@best-inc.com>
2025-12-20 21:32:18 +0800

    SQL格式化 使用idea格式化

--- src/main/java/com/alan/plugins/MyBatisLogFormatter/actions/MyBatisLogFormatterToQueryConsole.java
+++ src/main/java/com/alan/plugins/MyBatisLogFormatter/actions/MyBatisLogFormatterToQueryConsole.java
@@ -58,7 +58,7 @@ public class MyBatisLogFormatterToQueryConsole extends AnAction {
                             project,
                             selectedDataSource,
                             selectedNamespace,
-                            SqlUtils.beautifySql(sqlContent)
+                            sqlContent
                     );
                 })
         );
--- src/main/java/com/alan/plugins/MyBatisLogFormatter/handler/OpenQueryConsoleActionHandler.java
+++ src/main/java/com/alan/plugins/MyBatisLogFormatter/handler/OpenQueryConsoleActionHandler.java
@@ -5,12 +5,20 @@ import com.alan.plugins.MyBatisLogFormatter.notice.NotificationHelper;
 import com.intellij.database.dataSource.LocalDataSource;
 import com.intellij.database.editor.DatabaseEditorHelper;
 import com.intellij.database.model.DasNamespace;
-import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ModalityState;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.ScrollType;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.codeStyle.CodeStyleManager;
 
 /**
  * @author Alan
@@ -45,18 +53,39 @@ public class OpenQueryConsoleActionHandler {
             VirtualFile file = DatabaseEditorHelper.createNewConsoleVirtualFile(selectedDataSource);
             if (file != null) {
                 DatabaseEditorHelper.openConsoleForFile(project, selectedDataSource, namespace, file);
-                Document document = FileDocumentManager.getInstance().getDocument(file);
-                if (document != null) {
-                    // 获取当前应用实例
-                    Application application = ApplicationManager.getApplication();
-                    // 使用 runWriteAction 执行写操作
-                    application.runWriteAction(() -> {
-                        // 设置新的内容
+
+                // 等待 editor/psi 准备好后再写入并格式化（使用 IDEA 自带 SQL 格式化规则）
+                ApplicationManager.getApplication().invokeLater(() -> {
+                    if (project.isDisposed()) {
+                        return;
+                    }
+                    Document document = FileDocumentManager.getInstance().getDocument(file);
+                    if (document == null) {
+                        return;
+                    }
+
+                    WriteCommandAction.runWriteCommandAction(project, () -> {
                         document.setText(sqlContent);
+                        PsiDocumentManager.getInstance(project).commitDocument(document);
+
+                        PsiFile psiFile = PsiManager.getInstance(project).findFile(file);
+                        if (psiFile != null) {
+                            CodeStyleManager.getInstance(project).reformat(psiFile);
+                        }
                     });
-                    // 保存文档
+
+                    // 尽量让光标落在该 Console 的编辑器中
+                    Editor editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
+                    if (editor != null) {
+                        VirtualFile currentFile = FileDocumentManager.getInstance().getFile(editor.getDocument());
+                        if (file.equals(currentFile)) {
+                            editor.getCaretModel().moveToOffset(0);
+                            editor.getScrollingModel().scrollToCaret(ScrollType.CENTER_UP);
+                        }
+                    }
+
                     FileDocumentManager.getInstance().saveDocument(document);
-                }
+                }, ModalityState.defaultModalityState());
             }
         } catch (Exception e) {
             NotificationHelper.showErrorNotification(project, I18nBundle.message("label.open.queryConsole.error")+ "\n" + e.getMessage());
