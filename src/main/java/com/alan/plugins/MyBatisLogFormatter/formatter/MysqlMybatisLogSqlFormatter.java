10ea2d2d6f27fabd5f08842c83e2eb12e587188e
duanxq <xqduan@best-inc.com>
2025-12-21 22:13:42 +0800

    Refactor switch expressions in SQL formatters

--- src/main/java/com/alan/plugins/MyBatisLogFormatter/formatter/MysqlMybatisLogSqlFormatter.java
+++ src/main/java/com/alan/plugins/MyBatisLogFormatter/formatter/MysqlMybatisLogSqlFormatter.java
@@ -7,15 +7,15 @@ public class MysqlMybatisLogSqlFormatter extends AbstractMybatisLogSqlFormatter 
 
     @Override
     protected String formatParameterValue(String value, String type) {
-        switch (type.toLowerCase()) {
-            case "string", "char":
-            case "date", "time", "localdatetime", "localdate", "timestamp":
-                return formatString(value);
-            default:
+        return switch (type.toLowerCase()) {
+            case "string", "char",
+                 "date", "time",
+                 "localdatetime", "localdate", "timestamp" -> formatString(value);
+            default ->
                 // 数字
                 // 复杂类型。比如 ByteArrayInputStream
-                return value;
-        }
+                    value;
+        };
     }
 }
 
--- src/main/java/com/alan/plugins/MyBatisLogFormatter/formatter/OracleMybatisLogSqlFormatter.java
+++ src/main/java/com/alan/plugins/MyBatisLogFormatter/formatter/OracleMybatisLogSqlFormatter.java
@@ -7,22 +7,21 @@ public class OracleMybatisLogSqlFormatter extends AbstractMybatisLogSqlFormatter
 
     @Override
     protected String formatParameterValue(String value, String type) {
-        switch (type.toLowerCase()) {
-            case "time", "localtime":
-                return String.format("to_date('1970-01-01 %s', 'yyyy-MM-dd HH24:MI:ss')", value);
-            case "timestamp", "localdatetime": // Oracle 时间戳需要使用 to_timestamp 函数
-                return String.format("to_timestamp('%s', 'yyyy-MM-dd HH24:MI:ss.ff')", value);
-            case "date", "localdate": // Oracle 日期需要使用 to_date 函数
-                return String.format("to_date('%s', 'yyyy-MM-dd')", value);
-            case "boolean":
-                return formatBoolean(value);
-            case "string", "char":
-                return formatString(value);
-            default:
+        return switch (type.toLowerCase()) {
+            case "time", "localtime" -> String.format("to_date('1970-01-01 %s', 'yyyy-MM-dd HH24:MI:ss')", value);
+            case "timestamp", "localdatetime" ->
+                // Oracle 时间戳需要使用 to_timestamp 函数
+                    String.format("to_timestamp('%s', 'yyyy-MM-dd HH24:MI:ss.ff')", value);
+            case "date", "localdate" ->
+                // Oracle 日期需要使用 to_date 函数
+                    String.format("to_date('%s', 'yyyy-MM-dd')", value);
+            case "boolean" -> formatBoolean(value);
+            case "string", "char" -> formatString(value);
+            default ->
                 // 数字
                 // 复杂类型。比如 ByteArrayInputStream
-                return value;
-        }
+                    value;
+        };
     }
 }
 
